89f90c7756a387323fa4f2e144916205e6594ff1
sean-bradbury <sean.m.bradbury@googlemail.com>
2020-12-14 15:38:08 +0000

    Changed JS nav to checkbox nav

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -5,23 +5,18 @@ import "../styles/components/nav/nav.css"
 
 export default function Nav(props){
 
-    function menuClick() {
-        const navLinks = document.querySelector(".nav-links-container");
-        const hamburger = document.querySelector(".hamburger-container");
-        
-        navLinks.classList.toggle("active");
-        hamburger.classList.toggle("active");
-    }
-
     return(
         <Slide left delay={700}>
             <div>        
                 <nav id="nav">
+                    <input type="checkbox" class="toggle"/>
+                    
                     <div className="hamburger-container">
-                        <div onClick={menuClick} className="hamburger">                        
+                        <div className="hamburger">
                             <div className="hamburger-line"></div>
                         </div>
                     </div>
+
                     <div className="nav-links-container">
                         <Link className="nav-link" to="/"><img className="nav-link-image" src="../img/nav-icons/home.svg" alt=""/><div className="nav-link-text">Home</div></Link>
                         <Link className="nav-link" to="/about/"><img className="nav-link-image" src="../img/nav-icons/about.svg" alt=""/><div className="nav-link-text">About</div></Link>
