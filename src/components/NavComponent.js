ee43e6e2c7f69f542d7cc7c7a3780535db13bdca
sean-bradbury <sean.m.bradbury@googlemail.com>
2021-01-05 18:37:42 +0000

    Added active state to nav

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -21,7 +21,7 @@ export default function Nav(props){
 
                         <div className="nav-links-container">
                             <Link className="nav-link" to="/"><img className="nav-link-image" src="../img/nav-icons/home.svg" alt=""/><div className="nav-link-text">Home</div></Link>
-                            <Link className="nav-link" to="/about/"><img className="nav-link-image" src="../img/nav-icons/about.svg" alt=""/><div className="nav-link-text">About</div></Link>
+                            <Link className="nav-link" to="/about/"><img className="nav-link-image" src="../img/nav-icons/about.svg" alt=""/><div className="nav-link-text active">About</div></Link>
                             <Link className="nav-link" to="/skills/"><img className="nav-link-image" src="../img/nav-icons/skills.svg" alt=""/><div className="nav-link-text">Skills</div></Link>
                             <Link className="nav-link" to="/portfolio/"><img className="nav-link-image" src="../img/nav-icons/portfolio.svg" alt=""/><div className="nav-link-text">Portfolio</div></Link>
                             <Link className="nav-link" to="/contact/"><img className="nav-link-image" src="../img/nav-icons/contact.svg" alt=""/><div className="nav-link-text">Contact</div></Link>
