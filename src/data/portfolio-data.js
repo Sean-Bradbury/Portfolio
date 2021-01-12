c229029d2a14c6c792964bc3c43b2d6357e2dfed
sean-bradbury <sean.m.bradbury@googlemail.com>
2021-01-12 13:37:18 +0000

    Re-shuffle portfolio items

--- src/data/portfolio-data.js
+++ src/data/portfolio-data.js
@@ -9,22 +9,6 @@ const PortfolioData = [
     },
     {
         "id": 2,
-        "title": "Rest Countries API",
-        "tech": "React, Javascript, API, CSS, HTML",
-        "viewSiteLink": "https://stupefied-ride-ab7e18.netlify.app/",
-        "viewSiteCode": "https://github.com/Sean-Bradbury/rest-countries-api",
-        "image": "../img/portfolio/rest-countries-api.png"
-    },    
-    {
-        "id": 3,
-        "title": "Slumba",
-        "tech": "Design, Logo, HTML, SCSS, JS, JQuery",
-        "viewSiteLink": "https://www.slumba.online",
-        "viewSiteCode": "https://github.com/Sean-Bradbury/slumba",
-        "image": "../img/portfolio/slumba-website.png"
-    },
-    {
-        "id": 4,
         "title": "JW Trees",
         "tech": "Design, HTML, SCSS, JS, JQuery",
         "viewSiteLink": "https://www.jwtrees.co.uk/",
@@ -32,7 +16,15 @@ const PortfolioData = [
         "image": "../img/portfolio/jw-website.png"
     },
     {
-        "id": 5,
+        "id": 3,
+        "title": "Rest Countries API",
+        "tech": "React, Javascript, API, CSS, HTML",
+        "viewSiteLink": "https://stupefied-ride-ab7e18.netlify.app/",
+        "viewSiteCode": "https://github.com/Sean-Bradbury/rest-countries-api",
+        "image": "../img/portfolio/rest-countries-api.png"
+    },    
+    {
+        "id": 4,
         "title": "H20 Property Services",
         "tech": "Design, Logo, HTML, PHP, Bootstrap",
         "viewSiteLink": "https://sheltered-fortress-18792.herokuapp.com/",
@@ -40,20 +32,28 @@ const PortfolioData = [
         "image": "../img/portfolio/h2o-website.png"
     },
     {
-        "id": 6,
-        "title": "Learn To Code",
-        "tech": "HTML, SCSS, Form Validation",
-        "viewSiteLink": "https://amazing-swirles-1b3f9f.netlify.app/",
-        "viewSiteCode": "https://github.com/Sean-Bradbury/Intro-Component-SignUp",
-        "image": "../img/portfolio/learntocode-landingpage.png"
+        "id": 5,
+        "title": "Slumba",
+        "tech": "Design, Logo, HTML, SCSS, JS, JQuery",
+        "viewSiteLink": "https://www.slumba.online",
+        "viewSiteCode": "https://github.com/Sean-Bradbury/slumba",
+        "image": "../img/portfolio/slumba-website.png"
     },
     {
-        "id": 7,
+        "id": 6,
         "title": "Bookmark",
         "tech": "HTML, SCSS, JS, JQuery, Form Validation",
         "viewSiteLink": "https://peaceful-lamport-ff3a3e.netlify.app/",
         "viewSiteCode": "https://github.com/Sean-Bradbury/Bookmark-LP",
         "image": "../img/portfolio/bookmark-landingpage.png"
+    },
+    {
+        "id": 7,
+        "title": "Learn To Code",
+        "tech": "HTML, SCSS, Form Validation",
+        "viewSiteLink": "https://amazing-swirles-1b3f9f.netlify.app/",
+        "viewSiteCode": "https://github.com/Sean-Bradbury/Intro-Component-SignUp",
+        "image": "../img/portfolio/learntocode-landingpage.png"
     }
 ];
 
