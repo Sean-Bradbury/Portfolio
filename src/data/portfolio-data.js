8204c2c15fb7a765c2f9f37d08a4288b865f4ed6
Sean Brabdury <sean.bradbury@remarkable.net>
2023-05-04 20:22:50 +0100

    added entertainment app

--- src/data/portfolio-data.js
+++ src/data/portfolio-data.js
@@ -17,6 +17,14 @@ const PortfolioData = [
   },
   {
     id: 3,
+    title: "Entertainment Web App",
+    tech: "React, Typescript, SASS, HTML",
+    viewSiteLink: "https://sb-entertainment-app.netlify.app/",
+    viewSiteCode: "https://github.com/Sean-Bradbury/entertainment-web-app",
+    image: "../img/portfolio/entertainment-app.png",
+  },
+  {
+    id: 4,
     title: "Interactive Comment Section",
     tech: "React, Javascript, SASS, HTML",
     viewSiteLink: "https://sb-comment-section.netlify.app/",
@@ -25,7 +33,7 @@ const PortfolioData = [
     image: "../img/portfolio/sb_comment_section.png",
   },
   {
-    id: 4,
+    id: 5,
     title: "Rest Countries API",
     tech: "React, Javascript, API, CSS, HTML",
     viewSiteLink: "https://stupefied-ride-ab7e18.netlify.app/",
@@ -33,7 +41,7 @@ const PortfolioData = [
     image: "../img/portfolio/rest-countries-api.png",
   },
   {
-    id: 5,
+    id: 6,
     title: "H20 Property Services",
     tech: "Design, Logo, HTML, PHP, Bootstrap",
     viewSiteLink: "http://www.h2oplumbingandgas.co.uk/",
@@ -41,7 +49,7 @@ const PortfolioData = [
     image: "../img/portfolio/h2o-website.png",
   },
   {
-    id: 6,
+    id: 7,
     title: "Slumba",
     tech: "Design, Logo, HTML, SCSS, JS, JQuery",
     viewSiteLink: "https://slumba.netlify.app/",
@@ -49,7 +57,7 @@ const PortfolioData = [
     image: "../img/portfolio/slumba-website.png",
   },
   {
-    id: 7,
+    id: 8,
     title: "Bookmark",
     tech: "HTML, SCSS, JS, JQuery, Form Validation",
     viewSiteLink: "https://peaceful-lamport-ff3a3e.netlify.app/",
@@ -57,7 +65,7 @@ const PortfolioData = [
     image: "../img/portfolio/bookmark-landingpage.png",
   },
   {
-    id: 8,
+    id: 9,
     title: "Learn To Code",
     tech: "HTML, SCSS, Form Validation",
     viewSiteLink: "https://amazing-swirles-1b3f9f.netlify.app/",
